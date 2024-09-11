83b0565982f0068df64c2ed44fe108f0e351015c
Kauan Constancio <kauanconstancio13@gmail.com>
2024-09-11 12:58:53 -0300

    added script and animation code

---
+++ scrpit.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+    const body = document.body
+    body.classList.toggle('light-mode')
+
+    // if(body.classList.contains('light-mode')) {
+    //     body.classList.remove('light-mode')
+    // } else {
+    //     body.classList.add('light-mode')
+    // }
+
+}
